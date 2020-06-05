2b0b21d392a9308f1f96950ce4c0d5c374d3f1d1
huks <wkdgur1209@gmail.com>
2020-06-05 18:19:54 +0900

    구구단 수정

--- 01.vue/Web.vue
+++ 01.vue/Web.vue
@@ -1,24 +1,42 @@
 <template>
-  <div>
-    <h1>구구단</h1>
-    <div>{{ first }} x {{ second }} ?</div>
-    <form :onSubmit="calcGugudan">
-      <input type="text" v-model="input" />
-      <button type="submit">계산</button>
-    </form>
-    <h1>{{ result }}</h1>
-  </div>
+	<div>
+		<h1>구구단</h1>
+		<div>{{ first }} x {{ second }} ?</div>
+		<form @submit="calcGugudan">
+			<input type="text" v-model="input" ref="answer" />
+			<button type="submit">계산</button>
+		</form>
+		<h1>{{ result }}</h1>
+	</div>
 </template>
 <script>
-export default {
-  data: {
-    first: Math.ceil(Math.random() * 9),
-    second: Math.ceil(Math.random() * 9),
-    input: '',
-    result: '',
-  },
-  methods: {
-    calcGugudan() {},
-  },
-}
+	export default {
+		data: {
+			first: Math.ceil(Math.random() * 9),
+			second: Math.ceil(Math.random() * 9),
+			input: "",
+			result: ""
+		},
+		methods: {
+			calcGugudan(e) {
+				e.preventDefault();
+				const numberChk = /^[0-9]*$/;
+				console.log(this.input, numberChk.test(this.input));
+				if (numberChk.test(this.input) === false) {
+					this.result = "숫자만 입력하세요";
+				} else {
+					if (this.first * this.second === parseInt(this.input)) {
+						this.result = "정답";
+						this.first = Math.ceil(Math.random() * 9);
+						this.second = Math.ceil(Math.random() * 9);
+					} else {
+						this.result = "오답";
+					}
+				}
+
+				this.input = "";
+				this.$refs.answer.focus;
+			}
+		}
+	};
 </script>
