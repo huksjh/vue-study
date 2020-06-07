098edd95301a906eeb54ea7db1ab6dc956ef4d20
huks <wkdgur1209@gmail.com>
2020-06-07 22:06:27 +0900

    틀 테스트

---
+++ 02.web/src/views/layout/head.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<v-app-bar app color="deep-purple accent-4" dark>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-toolbar-title>Page title</v-toolbar-title>
+			<!-- spacer 뒤에꺼 오른쪽으로 정렬 -->
+			<v-spacer></v-spacer>
+			<v-btn icon to="/about">
+				<v-icon>mdi-magnify</v-icon>
+				<!-- 돋보기 아이콘 -->
+			</v-btn>
+		</v-app-bar>
+		<v-navigation-drawer app fixed temporary v-model="drawer">
+			<v-list-item>
+				<v-list-item-content>
+					<v-list-item-title class="title">Application</v-list-item-title>
+					<v-list-item-subtitle>subtext</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+
+			<v-divider></v-divider>
+		</v-navigation-drawer>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				drawer: false
+			};
+		}
+	};
+</script>
+
+<style>
+</style>
