07f26b1ebe862c00285670a850a5f6df2962e734
huks <wkdgur1209@gmail.com>
2020-06-09 00:29:40 +0900

    텍스트수정

--- 02.web/src/plugins/firebase.js
+++ 02.web/src/plugins/firebase.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import * as firebase from 'firebase/app';
 import 'firebase/auth'; //인증
 import 'firebase/firebase-database'; //데이터베이스
-import firebaseConfig from '../../firebaseConfig';
+import firebaseConfig from '../../firebaseConfig'; //키값 설정불러오기
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
