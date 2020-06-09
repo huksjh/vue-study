52dca7094b3f31f51ef8ebc93ab56155366c0d32
huks <wkdgur1209@gmail.com>
2020-06-09 10:04:36 +0900

    텍스트수정

--- 02.web/src/plugins/firebase.js
+++ 02.web/src/plugins/firebase.js
@@ -1,10 +1,16 @@
+//vue 초기셋팅
 import Vue from 'vue';
+//파이어베이서 app 기능 불러오기
 import * as firebase from 'firebase/app';
-import 'firebase/auth'; //인증
-import 'firebase/firebase-database'; //데이터베이스
-import firebaseConfig from '../../firebaseConfig'; //키값 설정불러오기
-
-// Initialize Firebase
+//파이어베이스 기능중 필요한 항목 설정
+//인증기능
+import 'firebase/auth';
+//데이터베이스
+import 'firebase/database';
+//파이어베이스 키값 설정
+import firebaseConfig from '../../firebaseConfig';
+//파이어베이스 install
 firebase.initializeApp(firebaseConfig);
 
-Vue.prototype.$firebase = firebase; //$firebase 전역적으로 사용하기위해
+//Vue 전역사용 $firebase 설정
+Vue.prototype.$firebase = firebase;
